f94619df185363dd3e35135ac45ad0d198ff891b
AlexandruAndrita <andu.andritza@gmail.com>
2025-10-13 20:21:24 +0300

    temporarily removed summary of each processed document

--- openai_service.py
+++ openai_service.py
@@ -51,8 +51,8 @@ def call_openai_with_images(image_urls: list[str]) -> dict:
         "- diagnostic_pacient\n"
         "Diagnosticul va aparea dupa cuvintele cheie: 'Diagnostic', 'Diagnosticul', 'Diagnostificat cu'\n\n"
 
-        "- sumar_document\n"
-        "Genereaza un rezumat detaliat al documentului prezentand etapele de investigatie, analizele facute de pacient, starea pacientului, tratamentele care trebuie urmate si diagnosticul. Daca unul din termenii anteriori nu se regaseste in document, nu il mentiona. Rezumatul trebuie sa aiba maxim 500 de caractere.\n\n"
+        # "- sumar_document\n"
+        # "Genereaza un rezumat detaliat al documentului prezentand etapele de investigatie, analizele facute de pacient, starea pacientului, tratamentele care trebuie urmate si diagnosticul. Daca unul din termenii anteriori nu se regaseste in document, nu il mentiona. Rezumatul trebuie sa aiba maxim 500 de caractere.\n\n"
     )
 
     user_content = [{"type": "text", "text": instruction}]
